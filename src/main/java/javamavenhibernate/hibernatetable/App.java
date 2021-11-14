3f5b7ab6a13b1f70a6e9c1d202b0c352629838a9
SreelakshmiSajeevan <54982096+SreelakshmiSajeevan@users.noreply.github.com>
2021-11-14 15:25:41 +0530

    Add files via upload

---
+++ src/main/java/javamavenhibernate/hibernatetable/App.java
@@ -0,0 +1,13 @@
+package javamavenhibernate.hibernatetable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
